d4762989b070d685011777c9697433f7c99d612a
unknown <406959526@qq.com>
2018-08-27 19:33:11 +0800

    first commit

---
+++ MatFun.jl
@@ -0,0 +1,35 @@
+﻿
+function eye(::Type{T}, m::Integer, n::Integer) where T
+    mab=min(m*n,m^2)
+    A=zeros(T,m,n);
+    for i=1:m+1:mab
+        A[i]=oneunit(T);        
+    end
+    return A
+end
+
+function eye(m::Integer,n::Integer) where T
+    A=eye(Float64,m,n);
+    return A
+end
+
+function eye(::Type{T}, m::Integer) where T
+    n=m;
+    A=eye(T,m,n);
+    return A
+end
+
+function eye(m::Integer) where T
+    n=m;
+    A=eye(Float64,m,n);
+    return A
+end
+"""
+# Example
+eye(3)
+
+eye(3,3)
+
+eye(Int64,2,3)
+
+"""
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# MatFun.jl
-rewrite some common matlab function into julia language
+      MatFun.jl is a set of common function rewrite in julia 1.0.if any bugs find in it,please report it on our github homepage.
+  
