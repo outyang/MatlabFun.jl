239e87236ef2b20bbd70587e78ce13214489dfc2
unknown <406959562@qq.com>
2018-10-04 10:58:53 +0800

    104

--- primes.jl
+++ primes.jl
@@ -1,25 +1,18 @@
-function primes(n::Int64)
-
-nlen=ceil(n/2);
-nlen=convert(Int64,nlen)
-p=trues(nlen);
-q = length(p);
-temp=sqrt(convert(Float64,n));
-ua = floor(sqrt(n));
-ub=convert(Int64,ua)
-for k = 3:2:ub
-  if p[div(k+1,2)]
-     for i=div(k*k+1,2):k:q
-         p[i] = false;
-     end
-  end
-end
-m=1:nlen;
-p=m[p];
-p=p.*2;
-p=p.-1;      
-p[1] = 2; 
-return p
+function primes2(maxn::Int64)
+    half = cld(maxn, 2)
+    p = trues(half)
+    for i = 3:2:isqrt(maxn)
+        if p[div(i + 1, 2)]
+            for j = div(i^2 + 1, 2):i:half
+                p[j] = false
+            end
+        end
+    end
+    p = (1:half)[p]
+    p .*= 2
+    p .-= 1
+    p[1] = 2
+    return p
 end
 
 """
